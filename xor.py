65ebbda89dda334baa33f0dfbd0134d67d777db0
Chihoko Ueno <harrierraum@hotmail.com>
2020-10-23 21:50:07 +0200

    add fitting

--- xor.py
+++ xor.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Activation, Dense
+import numpy as np
 
 def main():
   model = Sequential()
@@ -7,7 +8,16 @@ def main():
   model.add(Activation('sigmoid'))
   model.add(Dense(1))
   model.add(Activation('sigmoid'))
-  model.summary()
+  # model.summary()
+  model.compile(
+          optimizer="adam",
+          loss="mse",
+          metrics=["accuracy"])
+  x = np.array([[0, 0], [0, 1], [1, 0], [1,1]])
+  y = np.array([0, 1 , 1, 0])
+  model.fit(x, y, epochs=5000)
+  result = model.predict(x)
+  print(result)
 
 if __name__ == "__main__":
   main()
